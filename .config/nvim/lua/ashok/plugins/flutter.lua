d27ec7f84da56f9e314cc88933b54ac013eefb59
pokhrelashok2 <pokhrelashok2@gmail.com>
2024-08-18 12:31:48 +0545

    Flutter colors added

--- .config/nvim/lua/ashok/plugins/flutter.lua
+++ .config/nvim/lua/ashok/plugins/flutter.lua
@@ -4,5 +4,18 @@ return {
 		"nvim-lua/plenary.nvim",
 		"stevearc/dressing.nvim",
 	},
-	config = true,
+	config = function()
+		local ft = require("flutter-tools")
+		ft.setup({
+			ui = {
+				border = "rounded",
+			},
+			lsp = {
+				color = {
+					enabled = true,
+					background = true,
+				},
+			},
+		})
+	end,
 }
