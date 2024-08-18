0e49577e414ec259fad088011c196996263a6577
pokhrelashok2 <pokhrelashok2@gmail.com>
2024-08-18 10:23:31 +0545

    Themes added for alacritty & nvim:

---
+++ .config/nvim/lua/ashok/plugins/theme-switcher.lua
@@ -0,0 +1,25 @@
+return {
+	"zaldih/themery.nvim",
+	config = function()
+		local themery = require("themery")
+		themery.setup({
+			themes = {
+				{
+					name = "tokyonight",
+					colorscheme = "tokyonight",
+				},
+				{
+					name = "catpuccin",
+					colorscheme = "catppuccin",
+				},
+				{
+					name = "rose-pine",
+					colorscheme = "rose-pine",
+				},
+			},
+		})
+		local keymap = vim.keymap
+
+		keymap.set("n", "<leader>t", "<cmd>Themery<cr>", { desc = "Switch Themes" })
+	end,
+}
