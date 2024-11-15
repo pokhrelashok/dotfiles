4c63cb26067763596c59130dd4d4ac0fa493ca68
pokhrelashok2 <pokhrelashok2@gmail.com>
2024-11-15 16:02:36 +0545

    Autotag added

--- .config/nvim/lua/ashok/plugins/autotag.lua
+++ .config/nvim/lua/ashok/plugins/autotag.lua
@@ -1,6 +1,13 @@
 return {
-	"windwp/nvim-ts-autotag",
+	"nvim-ts-autotag",
 	config = function()
-		require("nvim-ts-autotag").setup()
+		local ts = require("nvim-ts-autotag")
+		ts.setup({
+			opts = {
+				enable_rename = true,
+				enable_close = true,
+				enable_close_on_slash = true,
+			},
+		})
 	end,
 }
